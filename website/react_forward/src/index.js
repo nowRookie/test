17e20c5b8adbca69b167b8e919edb7df4a2a41d1
luyi2 <luyi@carsvs.com>
2020-03-26 18:36:30 +0800

    structure adapt . bug:@font-face

--- website/react_forward/src/index.js
+++ website/react_forward/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import "./assets/css/index.scss"
+import "./assets/css/home.scss"
 
 ReactDOM.render(
   <React.StrictMode>
--- website/react_forward/src/views/Home.jsx
+++ website/react_forward/src/views/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react"
 
 export default class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-    render() {
-        return (<div>home</div>)
-    }
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+	handleClick(e) {
+	}
+	render() {
+		return (
+			<div className="home" onClick={this.handleClick}>
+				<div className="item">1111</div>
+			</div>
+		)
+	}
 }
