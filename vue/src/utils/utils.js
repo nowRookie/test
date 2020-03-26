d9af5406772202c01a58e1f3c28a935de0a693c7
luyi2 <luyi@carsvs.com>
2020-03-26 14:22:30 +0800

    structure adapt

--- vue/src/utils/utils.js
+++ vue/src/utils/utils.js
@@ -103,3 +103,63 @@ export function dealDate({ value, format } = {}) {
 export function calculate(val) {
   return moment(val * 1000).format("YYYY-MM-DD HH:mm:ss");
 }
+// 监测浏览器
+export function checkBrowser() {
+  const browser = {
+    versions: (function () {
+      var u = navigator.userAgent,
+        app = navigator.appVersion;
+      return {
+        //移动终端浏览器版本信息
+        trident: u.indexOf("Trident") > -1, //IE内核
+        presto: u.indexOf("Presto") > -1, //opera内核
+        webKit: u.indexOf("AppleWebKit") > -1, //苹果、谷歌内核
+        gecko: u.indexOf("Gecko") > -1 && u.indexOf("KHTML") == -1, //火狐内核
+        mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+        android: u.indexOf("Android") > -1 || u.indexOf("Linux") > -1, //android终端或uc浏览器
+        iPhone: u.indexOf("iPhone") > -1, //是否为iPhone或者QQHD浏览器
+        iPad: u.indexOf("iPad") > -1, //是否iPad
+        webApp: u.indexOf("Safari") == -1 //是否web应该程序，没有头部与底部
+      };
+    })(),
+    language: (
+      navigator.browserLanguage || navigator.language
+    ).toLowerCase()
+  };
+
+  if (browser.versions.mobile) {
+    //判断是否是移动设备打开。browser代码在下面
+    let ua = navigator.userAgent.toLowerCase(); //获取判断用的对象
+    console.log("navigator===", ua, "browser===", browser);
+    if (ua.match(/MicroMessenger/i) == "micromessenger") {
+      console.log("在微信中打开");
+      if (!localStorage.getItem("gOpenId")) {
+        this.getCode();
+      }
+    }
+    if (ua.match(/WeiBo/i) == "weibo") {
+      console.log("在新浪微博客户端打开");
+    }
+    if (ua.match(/QQ/i) == "qq") {
+      console.log("在QQ空间打开");
+    }
+    if (browser.versions.ios) {
+      console.log("在IOS浏览器打开");
+    }
+    if (browser.versions.android) {
+      console.log("在安卓浏览器打开");
+    }
+  } else {
+    console.log("PC浏览器打开");
+  }
+}
+// 读取input文件数据
+function reader() {
+  var input = document.getElementById("input")
+  var reader = new FileReader()
+  console.log(reader.readAsDataURL(input.files[0]))
+  reader.onload = function () {
+    console.log(reader.result);
+  }
+}
