34faf8d97c4aec798bb4e12fd236687928c3eca5
luyi <1073608648@qq.com>
2021-01-04 11:33:18 +0800

    adapt

--- umi/.umirc.ts
+++ umi/.umirc.ts
@@ -2,8 +2,14 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   // layout: {},//是否开启ant-design-pro风格
+  // node_modules下的文件不走babel编译
   nodeModulesTransform: {
     type: 'none',
+  },
+  // 整合 dva 数据流
+  dva: {
+    immer: true,//启用 immer 以方便修改 reducer
+    hmr: false,//启用 dva model 的热更新
   },
   // 配置别名，对引用路径进行映射，例如import "components"
   alias: {
@@ -18,4 +24,7 @@ export default defineConfig({
   // },
   // 快速刷新
   // fastRefresh: {}
+  // 忽略 moment 的 locale 文件，用于减少尺寸
+  ignoreMomentLocale: true,
+
 });
--- umi/src/pages/test.tsx
+++ umi/src/pages/test.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Slider, Switch, Button } from 'antd';
 import styles from './index.less';
 import Modal from "@/components/modal"
+import { connect } from "umi"
 
-const Test = () => {
+const Test = ({ global, dispatch }) => {
+    const [value, setvalue] = useState(0)
+    const [defaultValue, setdefaultValue] = useState(0)
+    const onChange = val => {
+        console.log(val)
+    }
+    const add = () => {
+        console.log(1111)
+        return value + 1
+    }
+    useEffect(() => {
+        // setTimeout(() => {
+        //     dispatch({
+        //         type: "global/save",
+        //         payload: { name: 123 }
+        //     })
+        // }, 3000)
+    }, [])
     return (
         <div>
             <Modal></Modal>
             <h1>test</h1>
+            <h3>{global.name}</h3>
+            <Slider defaultValue={defaultValue} max={32} min={26} onChange={onChange} />
+            <h1>{value}</h1>
+            <h2>{add()}</h2>
+            <Button onClick={() => { setvalue(value + 1) }}>点我+1</Button>
         </div>
     );
 }
-export default Test
+export default connect((scope) => {
+    // console.log("===============", scope)
+    let { global } = scope
+    return { global }
+})(Test)
