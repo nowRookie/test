ec48eba772803655fe430d22aa7de0090bfeac21
luyi <1073608648@qq.com>
2021-02-08 15:08:24 +0800

    adapt

--- website/vue/vue.config.js
+++ website/vue/vue.config.js
@@ -1,21 +1,22 @@
 const version = new Date().getTime();
-const webpack = require('webpack')
+const webpack = require("webpack");
 
 module.exports = {
+  lintOnSave: false, //如果为false，就是取消eslint规则的检查
   configureWebpack: {
     devtool: "source-map",
     entry: ["@babel/polyfill", "./src/main.js"],
     output: {
       filename: `js/[name].${version}.js`,
-      chunkFilename: `js/[name].${version}.js`
+      chunkFilename: `js/[name].${version}.js`,
     },
     plugins: [
       new webpack.ProvidePlugin({
         $: "jquery",
         jQuery: "jquery",
-        "windows.jQuery": "jquery"
-      })
-    ]
+        "windows.jQuery": "jquery",
+      }),
+    ],
   },
-  transpileDependencies: []
-}
+  transpileDependencies: [],
+};
